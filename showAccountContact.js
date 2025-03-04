d6f4154a8328636049414a5743e066293079733a
Vamshi Reddy Kasarla <143780580+vamshireddykasarla@users.noreply.github.com>
2025-03-04 13:29:11 -0600

    Add files via upload

--- showAccountContact.js
+++ showAccountContact.js
@@ -1,7 +1,12 @@
-import { LightningElement,track,wire } from 'lwc';
+import { LightningElement,track,wire, api } from 'lwc';
 import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
 import Marvel from '@salesforce/messageChannel/Marvel__c';
 import getContact from '@salesforce/apex/AccountClass.getContact';
+import LightningConfirm from 'lightning/confirm';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+
 
 export default class ShowAccountContact extends LightningElement {
     connectedCallback(){
@@ -21,6 +26,13 @@ export default class ShowAccountContact extends LightningElement {
     title='Contacts';
     @track contacts;
     @track hasContacts;
+    isAccountSelected=false;
+    @track isAddContact=false;
+    @track isEditcontact=false;
+    editableAccountId;
+   
+    //isCancelEvent=false;
+    @api recordId;
     handleSubscribe(){
         if(!this.subscription){
             this.subscription = subscribe(this.messageContext, Marvel, 
@@ -29,7 +41,8 @@ export default class ShowAccountContact extends LightningElement {
                     this.accountId=message.accountId;   
                     this.accountName=message.accountName;
                     this.title=this.accountName +"'S Contact";
-                    this.getContacts();
+                    this.getContact=this.contacts;
+                    refreshApex(this.getContact);
                     console.log('ShowAccountContact handleSubscribe');
                     console.log('Account Id: '+this.accountId);
                     console.log('Account Name: '+this.accountName);
@@ -39,22 +52,77 @@ export default class ShowAccountContact extends LightningElement {
     }
     //there are two methods to read salesforce data one is wire and imperative method
     //but we are using imperitive method this time we will have two key word async and await
-    async getContacts(){
+   @wire(getContact, { accountId: '$accountId' })
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.contacts = data;
+            this.hasContacts = this.contacts.length > 0;
+            this.isAccountSelected = true;
+        } else if (error) {
+            console.error('Error fetching contacts:', error);
+            this.contacts = [];
+            this.hasContacts = false;
+        }
+    }
+    /*async getContacts(){
         try{
             const result = await getContact({accountId:this.accountId});
             this.contacts = result;
             this.hasContacts=this.contacts.length>0?true:false;
+            this.isAccountSelected=true;
 
             console.log('ShowAccountContact getContacts');
             console.log('Contacts: '+JSON.stringify(this.contacts));
         }
         catch(error){
             console.error('Error in getting contacts: '+error);
-        }}
+        }}*/
     handleUnsubscribe(){
         if(this.subscription){
             unsubscribe(this.subscription);
             this.subscription = null;
         }
     }
+    handleAddContact(event){
+        this.isAddContact=true;
+    }
+    handleCancelEvent(event){
+        this.isAddContact=false;
+    }
+    handleEditEvent(event){
+        this.isEditcontact=true;
+        this.editableAccountId=event.target.dataset.contactId;
+    }
+    handleCancelEditEvent(event){
+        this.isEditcontact=false;
+    }
+    async handleSuccess(event) {
+        this.isAddContact=false;
+        this.isEditcontact=false;
+        await this.getContacts();
+    }
+   async handleDelete(event){
+        this.editableAccountId=event.target.dataset.contactId;
+        const result = await LightningConfirm.open({
+            message: 'Are you sure want to delete it?',
+            variant: 'headerless',
+            label: 'this is the aria-label value',
+            // setting theme would have no effect
+        });
+        if(result){
+            let deleterecord=deleteRecord(this.editableAccountId);
+            this.showToast();
+            this.getContacts();
+        }
+    }
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Delete',
+            message:
+                'Contact Success fully deleted',
+        });
+        this.dispatchEvent(event);
+    }
+    
+
 }
